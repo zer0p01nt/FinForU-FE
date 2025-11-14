39070c8699eac0ca9e0c7d3d9cdea73fd97e01f5
zer0p01nt <1027.bmy@gmail.com>
2025-11-14 08:49:08 +0000

    🐛Fix: PWA 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -31,11 +31,13 @@ export default defineConfig({
             src: "favicons/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any maskable",
           },
           {
             src: "favicons/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable",
           },
         ],
       },
